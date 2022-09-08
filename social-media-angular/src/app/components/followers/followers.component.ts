e441340873715180639ed9e3c4cd55d49b555a67
Roman <skinsrip21@liv.com>
2022-09-08 15:12:12 -0400

    Follow button

--- social-media-angular/src/app/components/followers/followers.component.ts
+++ social-media-angular/src/app/components/followers/followers.component.ts
@@ -18,17 +18,17 @@ export class FollowersComponent implements OnInit {
   })
 
   constructor(private followersService : FollowersService, private router : Router) { }
-
+  public isFollow: boolean = false;
   ngOnInit(): void {
   }
 
-
-
-
-  addTofollowList(){
-    this.followersService.follow().subscribe
+  onClick(){
+    this.isFollow = !this.isFollow;
 
   }
+
+
+
 
   onSubmit(e: any): void{
     e.preventDefault;
--- social-media-angular/src/app/components/register/register.component.ts
+++ social-media-angular/src/app/components/register/register.component.ts
@@ -15,13 +15,7 @@ export class RegisterComponent implements OnInit {
     lastName: new FormControl(''),
     email: new FormControl(''),
     password: new FormControl(''),
-<<<<<<< HEAD
-
-    username: new FormControl('')
-
-=======
     userName: new FormControl('')
->>>>>>> 9d90448fb3401dcf834742d5d39334e0cfd86fb5
   })
   
 
@@ -32,13 +26,7 @@ export class RegisterComponent implements OnInit {
   
   onSubmit(e: any): void {
     e.preventDefault()
-<<<<<<< HEAD
-
-    this.authService.register(this.registerForm.value.firstName || "", this.registerForm.value.lastName || "", this.registerForm.value.email || "", this.registerForm.value.password || "", this.registerForm.value.username || "")
-
-=======
     this.authService.register(this.registerForm.value.firstName || "", this.registerForm.value.lastName || "", this.registerForm.value.email || "", this.registerForm.value.password || "",this.registerForm.value.userName || "")
->>>>>>> 9d90448fb3401dcf834742d5d39334e0cfd86fb5
       .subscribe(
         (response) => {
           this.router.navigate(['login'])
