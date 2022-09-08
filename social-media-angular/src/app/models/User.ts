bddca11289f0078827e7c83e5215157030badd0a
steven246 <steven246@revature.net>
2022-09-08 10:28:43 -0500

    1128 s246 Users.ts

--- social-media-angular/src/app/models/User.ts
+++ social-media-angular/src/app/models/User.ts
@@ -3,23 +3,13 @@ export default class User {
     email: string
     firstName: string
     lastName: string
-<<<<<<< HEAD
-    username: string
-
-    constructor (id: number, email: string, firstName: string, lastName: string, username: string) {
-=======
     userName: string
 
     constructor (id: number, email: string, firstName: string, lastName: string, userName: string) {
->>>>>>> a1493a4c1c8a7631a02edbb9f572e523456936f4
         this.id = id
         this.email = email
         this.firstName = firstName
         this.lastName = lastName
-<<<<<<< HEAD
-        this.username = username
-=======
         this.userName = userName;
->>>>>>> a1493a4c1c8a7631a02edbb9f572e523456936f4
     }
 }
