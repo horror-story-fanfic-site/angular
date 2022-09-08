25afd07960e8e46ec34b613b77931389bb58a042
JFrazier <joshua.frazier1519@gmail.com>
2022-09-08 17:09:22 -0600

    fixed lost session on page refresh

--- social-media-angular/src/app/services/auth.service.ts
+++ social-media-angular/src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import User from '../models/User';
+import { LocalService } from './local.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,16 @@ import User from '../models/User';
 export class AuthService {
 
   authUrl: string = `${environment.baseUrl}/auth`;
-  currentUser: User
+  currentUser: User;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private localStorage: LocalService) { }
 
   login(email: string, password: string): Observable<any> {
     const payload = {email:email, password:password};
     const res = this.http.post<any>(`${this.authUrl}/login`, payload, {headers: environment.headers, withCredentials: environment.withCredentials});
     res.subscribe((data) => {
       this.currentUser = data
+      this.localStorage.saveData('user', JSON.stringify(data));
     })
     return res;
   }
@@ -31,4 +33,5 @@ export class AuthService {
     const payload = {firstName: firstName, lastName: lastName, email: email, password: password, username: username};
     return this.http.post<any>(`${this.authUrl}/register`, payload, {headers: environment.headers});
   }
+
 }
