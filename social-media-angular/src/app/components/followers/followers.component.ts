3a12db3c152f9b9fcb01445524e7e40ce34d5960
steven246 <steven246@revature.net>
2022-09-08 11:37:59 -0500

    1237 followers.component.ts s246

--- social-media-angular/src/app/components/followers/followers.component.ts
+++ social-media-angular/src/app/components/followers/followers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FollowersService } from 'src/app/services/followers.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-followers',
@@ -9,9 +10,24 @@ import { FollowersService } from 'src/app/services/followers.service';
 })
 export class FollowersComponent implements OnInit {
 
+  toFollowForm = new FormGroup({
+    email: new FormControl(''),
+  })
+
   constructor(private followersService : FollowersService, private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(e: any): void{
+    e.preventDefault;
+    // this.followersService.toFollowUsers(this.loginForm.value.email || "")
+    //   .subscribe(
+    //     (response) => {
+    //       this.followersService.currentUser = response
+    //       this.router.navigate(['toFollowUser'])
+    //     }
+    //   )
   }
 
 }
