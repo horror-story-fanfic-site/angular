f2424b6e3c1825121554f346ead8e73fb4db65a9
unknown <chandlercarnes@gmail.com>
2022-09-09 15:29:43 -0400

    implemented follower feed format and styles

--- social-media-angular/src/app/app-routing.module.ts
+++ social-media-angular/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FollowerFeedPageComponent } from './components/follower-feed-page/follower-feed-page.component';
 import { FollowersComponent } from './components/followers/followers.component';
 import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
 import { LoginComponent } from './components/login/login.component';
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: "profile", component: ProfileComponent },
   { path: "stalk", component: ProfileComponent },
   { path: "forgotpassword", component: ForgotpasswordComponent},
-  { path: "resetpassword", component: ResetpasswordComponent}
+  { path: "resetpassword", component: ResetpasswordComponent},
+  { path: "follower-feed", component: FollowerFeedPageComponent}
 ];
 
 @NgModule({
