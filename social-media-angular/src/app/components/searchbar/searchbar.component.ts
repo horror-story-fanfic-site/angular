c7154bc1ab5f1a6ca8aa338d1cbe786131db93c2
ceaselessphantom <90485964+ceaselessphantom@users.noreply.github.com>
2022-09-09 16:09:37 -0400

    to lower case

--- social-media-angular/src/app/components/searchbar/searchbar.component.ts
+++ social-media-angular/src/app/components/searchbar/searchbar.component.ts
@@ -66,7 +66,7 @@ export class SearchbarComponent implements OnInit {
 
     //This loops through all the usernames.
     for(let x=0;x<this.usernameList.length;x++){
-      let username=this.usernameList[x];
+      let username=this.usernameList[x].toLowerCase();
       let y, w: number;
 
       //Loop through all the characters in the search.
