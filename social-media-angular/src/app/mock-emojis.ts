e11797cd56ffdf019a6958e37e98d79e822bf7f2
JFrazier <joshua.frazier1519@gmail.com>
2022-09-10 15:04:54 -0600

    emoji buttons styled

--- social-media-angular/src/app/mock-emojis.ts
+++ social-media-angular/src/app/mock-emojis.ts
@@ -6,5 +6,5 @@ export const Emojis: Emoji[] = [
     {emojiId: 1, emojiName: 'thumbsup', emojiPic: 'https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f44d.png'},
     {emojiId: 2, emojiName: 'laughing', emojiPic: 'https://toppng.com/uploads/preview/emoji-transparent-laughing-emoji-11550234631y6jwxckdn1.png'},
     {emojiId: 3, emojiName: 'scared', emojiPic: 'https://toppng.com/uploads/preview/omg-face-emoji-png-sh-11545508210l9uqnywdn1.png'},
-    {emojiId: 4, emojiName: 'wow', emojiPic: 'https://e7.pngegg.com/pngimages/496/17/png-clipart-computer-icons-emoticon-smiley-like-button-smiley-miscellaneous-smiley.png'},
+    {emojiId: 4, emojiName: 'love', emojiPic: 'https://i.pinimg.com/736x/a2/7f/b7/a27fb7ad6ac81a1351cfe77597a9ecba.jpg'}
 ]
