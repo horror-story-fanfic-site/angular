12e908f21abfaf04f73127bf3d727608d633ea4e
Caleb Bousman <calebbousman@gmail.com>
2022-09-14 16:15:06 -0400

    dockerfile

--- social-media-angular/src/environments/environment.prod.ts
+++ social-media-angular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "http://localhost:9002",
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+  },
+  paramHeaders: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+  },
 };
