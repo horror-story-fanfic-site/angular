ee3e98d6b23acfac7584879b2450e60bfd66b77b
JFrazier <joshua.frazier1519@gmail.com>
2022-09-14 15:08:10 -0600

    hope

--- social-media-angular/src/environments/environment.ts
+++ social-media-angular/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   withCredentials: true,
-  baseUrl: "http://44.201.120.76/:9002",
+  baseUrl: "http://44.201.120.76:9002",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://44.201.120.76/:4200',
+    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200',
   },
   paramHeaders: {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Access-Control-Allow-Origin': 'http://44.201.120.76/:4200',
+    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200',
   },
 };
 
