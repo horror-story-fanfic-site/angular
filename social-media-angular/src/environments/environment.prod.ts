452281c246115a21ca34e5978cfdaa33fccd0009
steven246 <steven246@revature.net>
2022-09-15 10:26:11 -0500

    1126 build

--- social-media-angular/src/environments/environment.prod.ts
+++ social-media-angular/src/environments/environment.prod.ts
@@ -1,13 +1,16 @@
 export const environment = {
   production: true,
-  baseUrl: "http://44.201.120.76:9002",
+//  baseUrl: "http://44.201.120.76:9002",
+  baseUrl: "http://localhost:9002",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+//    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+    'Access-Control-Allow-Origin': 'http://localhost:4200/',
   },
   paramHeaders: {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+//    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+    'Access-Control-Allow-Origin': 'http://localhost:4200/',
   },
 };
--- social-media-angular/src/environments/environment.ts
+++ social-media-angular/src/environments/environment.ts
@@ -5,14 +5,17 @@
 export const environment = {
   production: false,
   withCredentials: true,
-  baseUrl: "http://44.201.120.76:9002",
+//  baseUrl: "http://44.201.120.76:9002",
+  baseUrl: "http://localhost:9002",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+//    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+    'Access-Control-Allow-Origin': 'http://localhost:4200/',
   },
   paramHeaders: {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+//    'Access-Control-Allow-Origin': 'http://44.201.120.76:4200/',
+    'Access-Control-Allow-Origin': 'http://localhost:4200/',
   },
 };
 
